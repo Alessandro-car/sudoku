befc58b1c4f84d4e3bb16ba5e0d4034aaa95b3ab
Pasquale121212 <pasquale.gesualdo124@gmail.com>
2025-06-12 22:40:37 +0200

    Add files via upload
    ho inserito la funzione convertire_numeri_in_lettere in .c e .h ed ho aggiunto i commenti sia alla mia funzione che alla funzione già presente: convertire_lettera_numero

--- src/utils.c
+++ src/utils.c
@@ -1,24 +1,26 @@
 #include "utils.h"
+#include "costanti.h"
 
 int convertire_lettera_in_numero(char lettera) {
+	//La seguente funzione permette di convertire le lettere passate in input in numeri
 		int numero;
-		numero = -1;
+		numero = -1; //Inizializza numero con -1
 
-		if (lettera >= 'A' && lettera <= 'G') {
+		if (lettera >= 'A' && lettera <= 'G') { //Effettua il controllo sulla lettera, deve essere essere un carattere che permette di ottenere un numero sottraendo 55
 			numero = lettera - 55;
-		} else if (lettera >= '0' && lettera <= '9') {
+		} else if (lettera >= '0' && lettera <= '9') { //Effettua il controllo sulla lettera, deve essere un carattere che permette di ottenere un numero sottraendo 48
 			numero = lettera - 48;
 		}
 		return numero;
 }
 
 char convertire_numeri_in_lettere(int numero) {
-
+// La seguente funzione permette di convertire i numeri passati da input in lettere
 	char lettera;
 
-	lettera = ' ';
-	if(numero >= 10 && numero<= DIMENSIONE_GRIGLIA_GRANDE) {
-		lettera = numero + 55;
+	lettera = ' '; //Inizializza il carattere in un carattere vuoto
+	if(numero >= 10 && numero<= DIMENSIONE_GRIGLIA_GRANDE) { // Effettua il controllo sul numero, deve rispettare i vincoli
+		lettera = numero + 55; // Somma al numero passato da input "55", questo permette di trasformarlo nella lettera corrispondente mediante ASCII
 	}
 	if(numero >= 0 && numero <= 9){
 		lettera = numero + 48;
