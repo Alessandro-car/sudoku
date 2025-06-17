54be87ab7bac24b5841d611c150d6c75b088dc2c
Pasquale121212 <pasquale.gesualdo124@gmail.com>
2025-06-17 22:34:48 +0200

    spostato in src utils.h

--- src/utils.h
+++ src/utils.h
@@ -12,5 +12,8 @@ int convertire_lettera_in_numero(char lettera);
 char convertire_numeri_in_lettere(int numero);
 partita caricare_partita_da_file(stringa nome_file_partita);
 int nascondere_input_utente();
+char convertire_minuscolo_maiuscolo(char lettera);
+
+
 
 #endif
